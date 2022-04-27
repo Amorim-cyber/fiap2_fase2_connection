2d1fb2e48e3ada105e6b10b3dcb2f9de9cbc60b3
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-04-27 10:48:49 -0300

    Primeiro commit
    Esse commit:
    
     - Utiliza a configuração determinada no site spring initializr
     - Adiciona as dependências webjars no arquivo pom.xml
     - Adiciona a dependência springdoc-openapi-ui no arquivo pom.xml
     - Renomea o arquivo application.properties para application.yml
     - Configura application.yml
     - Cria o bean do swaggerpara o arquivo de execução
    
    Closes #1

---
+++ src/test/java/br/com/fiap/showMeCurriculum/ShowMeCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.showMeCurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
