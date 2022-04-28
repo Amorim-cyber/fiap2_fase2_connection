facd0d14637e2f422f1a4ebef89eb934c460af04
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-04-28 11:00:13 -0300

    Adiciona RestController Certificacao [#3]
    Este commit:
    
     - Cria interface CertificadoRepository
     - Cria classe CertificacaoResource que implementa Repository
     - Adiciona uma nova mensagem de erro para Certificacao
     - Renomeia o atributo cdCandidato para cdCertificacao em Certificacao
    
    Closes #3

---
+++ src/main/java/br/com/fiap/showMeCurriculum/controller/resource/CertificacaoResource.java
@@ -0,0 +1,2 @@
+package br.com.fiap.showMeCurriculum.controller.resource;public class CertificacaoResource {
+}
---
+++ src/main/java/br/com/fiap/showMeCurriculum/repository/CertificadoRepository.java
@@ -0,0 +1,2 @@
+package br.com.fiap.showMeCurriculum.repository;public interface CertificadoRepository {
+}
